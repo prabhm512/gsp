8bdc5daa64a6829ed1e4fb273dd0bb158511b000
Prabh Singh <prabhm512@gmail.com>
2021-05-10 18:25:34 +1000

    basic page layout created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Grid } from '@material-ui/core';
+
 
 function App() {
+
+  const [state, setState] = useState("test");
+
+  const scrapeData = (event) => {
+    let url = event.target.parentNode.firstChild.value;
+    console.log(url);
+  }
+
+  const onInputChange = event => {
+    setState(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Search Prototype</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={12} style={{margin: "auto"}}>
+          <input value={state} onChange={onInputChange}></input>
+          <button onClick={scrapeData}>Search</button>
+        </Grid>
+      </Grid>
+      <div className="footer">
+        <h3>Prabh Singh, Trisha Raibal</h3>
+      </div>
     </div>
   );
 }
