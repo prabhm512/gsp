4934bba42427e20c476daf54a07ab7cb079a382f
Prabh Singh <prabhm512@gmail.com>
2021-05-14 16:34:17 +1000

    url validator created

---
+++ url-validator.js
@@ -0,0 +1,40 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const axios = require("axios");
+const csvWriter = require('csv-write-stream');
+
+// CSV writer
+let writer = csvWriter();
+writer.pipe(fs.createWriteStream('valid-urls-test.csv'), {flags: 'a'})
+
+// Store csv data
+const data = [];
+
+// Count number of valid rows
+let validCntr = 0;
+let invalidCntr = 0;
+
+fs.createReadStream('data/gsp_url_mapping1.csv')
+    .pipe(csv())
+    .on('data', async (row) => {
+        // Convert url to string
+        let url = row.url.toString();
+
+        await axios.get(url).then((response) => {
+            if (response.status == 200) {
+                
+                validCntr++;
+                console.log(row);
+
+                // Write records
+                // Add new column that conatains source code of valid url
+                writer.write({node_id: row.node_id, url: row.url, createdAt: row.createdAt, updatedAt: row.updatedAt, html: response.data})
+            }
+        }).catch(err => {
+            // Increment invalid url counter
+            invalidCntr++;
+        }).then(() => {
+            console.log("Valid: " + validCntr, "Invalid: " + invalidCntr);
+        })
+    })
+
